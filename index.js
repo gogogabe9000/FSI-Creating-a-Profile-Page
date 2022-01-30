7fa996ba3a48d8ef2a357d48cf57dc2c6373ae9d
gogogabe9000 <gabe.email1032@yahoo.com>
2022-01-30 12:02:58 -0500

    created a div element, still debugging the image and the div itself

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let content = document.querySelector('.js-generated.content')
-let header = document.createElement('h1')
+
+let header = document.createElement('h1') // scripts the dog name
 header.setAttribute('class', 'dog-name')
 header.append('Rizzo')
 content.append(header)
 
-let dogImage = document.createElement('img')
+let dogContent = document.createElement('div') // this creates a element in the div called dog content
+header.setAttribute('class', 'dog-content')
+
+let dogImage = document.createElement('img') //this is the image of the dog
 dogImage.setAttribute('class','dog-image')
 dogImage.setAttribute('class', './assets/rizzo.jpg')
