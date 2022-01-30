6ac80470a7242b1f74a62c6511fbbb80d1a4b39e
gogogabe9000 <gabe.email1032@yahoo.com>
2022-01-30 11:43:07 -0500

    set up the dog image, and debugging it, now working on the dog content

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+content.append(header)
+
+let dogImage = document.createElement('img')
+dogImage.setAttribute('class','dog-image')
+dogImage.setAttribute('class', './assets/rizzo.jpg')
